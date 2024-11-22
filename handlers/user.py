40449c08df899dbbe769d7c155b897808e21886c
ЗАХАР <zlit14@ya.ru>
2024-11-23 01:58:04 +0500

    added /start

--- handlers/user.py
+++ handlers/user.py
@@ -8,12 +8,23 @@ from aiogram.filters import CommandStart, Command, StateFilter
 from aiogram.fsm.context import FSMContext
 from sqlalchemy.ext.asyncio import AsyncSession
 
+
 # Созданные модули
 
 
-#------------------------ handlers ------------------------
+# ------------------------ handlers ------------------------
+
+async def start_handler(message: types.Message):
+    # Приветственное сообщение и инструкция
+    await message.answer(
+        "<i>Здравствуйте!</i>\n\n"
+        "Я <b>СКИТ help бот</b>, опишите свою проблему, и я постараюсь помочь.\n\n"
+        "▫Напишите подробно, чтобы я мог быстрее разобраться в вашем вопросе."
+    )
 
 
 ####################  Регистрируем хендлеры юзера  ####################
+
+
 def register_handlers(dp: Dispatcher):
-    pass
+    dp.message.register(start_handler, CommandStart())
