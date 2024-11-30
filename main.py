34d346ee05b48fd9fe7fd15cd0f27e9d8a9f5d2e
Zakhar Litvinenko <127947169+zlit14@users.noreply.github.com>
2024-11-30 12:02:34 +0500

    final

--- main.py
+++ main.py
@@ -5,11 +5,6 @@ import asyncio
 import os
 from datetime import datetime
 import logging
-logging.basicConfig(filename='skitbot.log',
-                    filemode='w',
-                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
-                    datefmt='%H:%M:%S',
-                    level=logging.DEBUG)
 # Сторонние модули
 from aiogram import Bot, Dispatcher, types
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -27,14 +22,13 @@ from api.skitAPI import api, connection, statuses, application_solution_format
 from database.application import Application
 from database.user import User
 
-
-
 bot = Bot(
-    token = config.BOT_TOKEN, parse_mode = "HTML",
-    disable_web_page_preview = True
+    token=config.BOT_TOKEN, parse_mode="HTML",
+    disable_web_page_preview=True
 )
 
 dp = Dispatcher()
+
 
 @connection
 async def applications_autoupdations(session: AsyncSession) -> None:
@@ -42,11 +36,11 @@ async def applications_autoupdations(session: AsyncSession) -> None:
 
         users_result = await session.execute(select(User))
         users: List[User] = list(users_result.scalars())
-        
+
         for user in users:
             applications_result = await session.execute(select(Application).where(Application.user_tgid == user.tgid))
             applications: List[Application] = list(applications_result.scalars())
-            
+
             for application in applications:
                 data = await api.get_application(application.id)
                 if data['status'] != application.status and data['status'] == statuses.DONE:
@@ -54,13 +48,13 @@ async def applications_autoupdations(session: AsyncSession) -> None:
                     await bot.send_message(int(user.tgid), answer)
                     application.status = statuses.DONE
                     await session.commit()
-                    
+
         await asyncio.sleep(int(config.AUTOUPDATE_TIME))
 
-                    
 
 async def on_startup():
     run_param = False  # Заменяем на True, если надо дропнуть БД
+    logging.basicConfig(level=logging.INFO, filename="py_log.log",filemode="w")
     if run_param:
         await drop_db()
     await create_db()
@@ -71,17 +65,17 @@ async def on_startup():
 
 async def main():
     dp.startup.register(on_startup)
-    dp.update.middleware(DataBaseSession(session_pool = session_maker))
-    await bot.delete_webhook(drop_pending_updates = True)
+    dp.update.middleware(DataBaseSession(session_pool=session_maker))
+    await bot.delete_webhook(drop_pending_updates=True)
 
-    await bot.set_my_commands(admin_commands, scope = types.BotCommandScopeAllGroupChats())
-    await bot.set_my_commands(user_commands, scope = types.BotCommandScopeAllPrivateChats())
+    await bot.set_my_commands(admin_commands, scope=types.BotCommandScopeAllGroupChats())
+    await bot.set_my_commands(user_commands, scope=types.BotCommandScopeAllPrivateChats())
 
     time_now = datetime.today()
     print(f"[{str(time_now)[:19]}]: Bot started")
 
     await asyncio.gather( dp.start_polling(bot), applications_autoupdations())
-    
+
 
 # Запуск бота
 if __name__ == "__main__":
@@ -90,7 +84,7 @@ if __name__ == "__main__":
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
         loop.run_until_complete(main())
-        
+
     except KeyboardInterrupt:
         print("Bot stopped")
 
